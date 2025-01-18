7b90f1b5c979ae7ae490c0dfe54397497ea326fb
Josias Gonçalves <156623562+josiasgdev@users.noreply.github.com>
2025-01-18 05:14:34 -0300

    Adicionada a lógica de criação de um arquivo denominado 'arquivo.hd para o registro dos chamados aberto em forma de string, a lógica foi adionada no script registra_chamado.php

---
+++ registra_chamado.php
@@ -0,0 +1,30 @@
+<?php
+echo '<pre>';
+print_r($_POST);
+
+/*
+O post é um array, o array recebido será formatado em um texto
+para que possa ser escrito dentro do arquivo.
+
+Os dados de cada campo do formulário recebidos através do método post serão separados
+pelo caractere '#', e, serão alocados na variável registro como string, através do método implode.
+*/
+$registro = implode('#', $_POST);
+str_replace('#', '-', $registro);
+
+/*Função nativa para abrir um novo arquivo. Caso o arquivo não exista, ele será criado
+primeiro é passado o nome do arquivo, depois a ação que será tomada com o arquivo, ex: escrita(a)*/
+
+
+$arquivo = fopen('arquivo.hd', 'a');
+
+//Espera dois parâmetros: a referência do arquivo aberto, o que será escrito no arquivo.
+//O PHP_EOL adiciona uma quebra de linha separando assim os chamados.
+fwrite($arquivo, $registro . PHP_EOL);
+
+//Fechar o arquivo.
+fclose($arquivo);
+
+echo $registro;
+
+header('Location: abrir_chamado.php');
