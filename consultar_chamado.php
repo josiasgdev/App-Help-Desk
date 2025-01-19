38e66d018269bbae7d7031efcb4b279b3c9374b6
Josias Gonçalves <156623562+josiasgdev@users.noreply.github.com>
2025-01-19 04:24:32 -0300

    Implementação da ĺógica de funcionamento da tela de consulta de chamados, através da conversão dos dados inseridos no arquivo.hd através do registro dos chamados.

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,5 +1,24 @@
 <?php require_once "validador_acesso.php"; ?>
 
+<?php
+//Armazenamento dos chamados
+$chamados = array();
+
+
+//abrir o arquivo.hd
+$arquivo = fopen("arquivo.hd", "r");
+
+/*feof testa pelo fim de um arquivo, a função retorna false enquanto não encontrar o final do arquivo, linha por linha, posicionando o curso no início da linha por isso é utlizada a negação lógica para que funcione como condicional do laço de repetição.*/
+while (!feof($arquivo)) {
+  /*fgets com base no documento aberto e na posição do cursor, recuperar o que estiver escrito até um determinado n° de bits ou até encontrar o final da linha(quebra de linha)*/
+  $registro = fgets($arquivo);
+ //Atribuindo cada arquivo lido e alocado em registro para um índice do array chamados
+  $chamados[] = $registro;  
+}
+//fechar o arquivo
+fclose($arquivo);
+?>
+
 <html>
 
 <head>
@@ -42,23 +61,25 @@
 
           <div class="card-body">
 
+          <?php foreach($chamados as $chamado) { ?>
+
+            <?php
+              $chamado_dados = explode('#', $chamado);
+              //Contagem da quantidade de elementos do array chamado_dados. Se for inferior a 3, é sinal de que falta algum item e a instrução é pulada
+              if(count($chamado_dados) < 3) {
+                continue;
+              }
+            ?>
             <div class="card mb-3 bg-light">
               <div class="card-body">
-                <h5 class="card-title">Título do chamado...</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                <p class="card-text">Descrição do chamado...</p>
+                <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                <p class="card-text"><?= $chamado_dados[2] ?></p>
 
               </div>
             </div>
 
-            <div class="card mb-3 bg-light">
-              <div class="card-body">
-                <h5 class="card-title">Título do chamado...</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                <p class="card-text">Descrição do chamado...</p>
-
-              </div>
-            </div>
+          <?php } ?>
 
             <div class="row mt-5">
               <div class="col-6">
