36ace3adffe07c33be392cd3b2d8e79cf59dc60d
Josias Gonçalves <156623562+josiasgdev@users.noreply.github.com>
2025-01-20 05:12:30 -0300

    Com a adição do id dos usuários no documento.hd e consequentemente no array , em consultar chamado, a exibição de título, categoria e descrição tiveram seus índices alterados.

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -72,9 +72,9 @@ fclose($arquivo);
             ?>
             <div class="card mb-3 bg-light">
               <div class="card-body">
-                <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
-                <p class="card-text"><?= $chamado_dados[2] ?></p>
+                <h5 class="card-title"><?= $chamado_dados[1] ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2] ?></h6>
+                <p class="card-text"><?= $chamado_dados[3] ?></p>
 
               </div>
             </div>
